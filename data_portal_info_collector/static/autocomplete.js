78e4ca365580aa568c8cd6263e0497ffd684682e
Erica Kwan <erica@codeforamerica.org>
2014-02-10 18:37:16 -0800

    Styling improvements that Mike wants

--- data_portal_info_collector/static/autocomplete.js
+++ data_portal_info_collector/static/autocomplete.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
         response( $.grep( places, function( item ){
             return matcher.test( item );
-        }) );
+        }).slice(0, 10) );
       }
     });
 });
